b884ec9d854e3d84307d23af0538ca39df22dd78
Felizardo Chirindja <felizardo.chirindja@gmail.com>
2024-11-30 11:42:27 +0200

    feat: implement todo selection and API integration using angular resources

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, effect, inject, ResourceStatus } from '@angular/core';
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tester';
+  todoService = inject(TodoService);
+
+  todos = this.todoService.todos;
+  todo = this.todoService.todo;
+
+  selectTodo(event: Event) {
+    const id = (event.target as HTMLInputElement).value;
+    
+    this.todoService.selectedTodoId.set(Number(id));
+  }
+
+  eff = effect(() => {
+    console.log(ResourceStatus[this.todo.status()]);
+    console.log(this.todo.value()?.todo);
+  });
+}
+
+export interface Todo {
+  id: number
+  todo: string
+  completed: boolean
+}
+
+export interface TodoResponse {
+  todos: Todo[]
+  total: number
+  skip: number
+  limit: number
 }
---
+++ src/app/todo.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { Todo, TodoResponse } from './app.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  private http = inject(HttpClient);
+  private readonly baseUrl = 'https://dummyjson.com/todos';
+
+  selectedTodoId = signal<number | null>(null);
+
+  todos = rxResource({
+    loader: () => this.http.get<TodoResponse>(this.baseUrl),
+  });
+
+  todo = rxResource({
+    request: () => this.selectedTodoId(),
+    loader: ({ request: id }) => this.http.get<Todo>(`${this.baseUrl}/${id}`),
+  });
+}
