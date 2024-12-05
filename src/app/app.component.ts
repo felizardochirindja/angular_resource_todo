b5414f5eea8368a8501369dc75c97f679af2ed01
Felizardo Chirindja <felizardo.chirindja@gmail.com>
2024-12-05 11:46:38 +0200

    compare declarative and imperative aproaches to react to template events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,34 @@
-import { Component, effect, inject, ResourceStatus } from '@angular/core';
+import { Component, effect, inject, OnDestroy, OnInit, ResourceStatus } from '@angular/core';
 import { TodoService } from './todo.service';
+import { map, Subject, takeUntil } from 'rxjs';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss', 
+  imports: [ReactiveFormsModule]
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
+
   todoService = inject(TodoService);
 
+  // data logic
   todos = this.todoService.todos;
   todo = this.todoService.todo;
 
+  // template logic
+  todoControl = new FormControl();
+
+  // react to changes in the select todo element using declarative approach
+  selectTodo$ = this.todoControl.valueChanges.pipe(
+    takeUntil(this.destroy$),
+    map(id => Number(id)),
+    map(id => this.todoService.selectedTodoId.set(id))
+  );
+
+  // react to changes in the select todo element using imperative approach
   selectTodo(event: Event) {
     const id = (event.target as HTMLInputElement).value;
     
@@ -22,6 +39,15 @@ export class AppComponent {
     console.log(ResourceStatus[this.todo.status()]);
     console.log(this.todo.value()?.todo);
   });
+
+  ngOnInit() {
+    this.selectTodo$.subscribe();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
 
 export interface Todo {
